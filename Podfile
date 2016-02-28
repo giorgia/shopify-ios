77b7d15b7759faac219f7fea718ff36f7bc562b8
Giorgia <giorgia.marenda@gmail.com>
2016-02-28 11:06:56 -0500

    Installed pods

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, 9
+
+use_frameworks!
+
+plugin 'cocoapods-keys', {
+  :project => 'shopify-ios',
+  :keys => [
+    'shopify-api-key',
+    'shopify-channel-id'
+  ]}
+
+# My extension pods
+pod 'Mobile-Buy-SDK', '~> 1.2.4'
+
+
+#social/tracking
+pod 'Fabric', '~> 1.6.6'
+pod 'Crashlytics', '~> 3.7.0'
---
+++ shopify-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopify-ios
+//
+//  Created by Giorgia Marenda on 2/27/16.
+//  Copyright © 2016 Giorgia Marenda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
